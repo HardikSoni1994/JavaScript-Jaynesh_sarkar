de934737048f04314fd2bf78ec1c5cae3b4d3fcb
HardikSoni1994 <soni.hardik994@gmail.com>
2025-12-24 20:34:49 +0530

    object

--- CH-4 Operators/4.1/comparison_operator.js
+++ CH-4 Operators/4.1/comparison_operator.js
@@ -1,10 +1,19 @@
-let age1 = 20;
-age2 = 20;
+let age1 = 17;
+age2 = 15;
 // comparison operator
 console.log(age1 != age2); // false
 console.log(age1 == age2); // true
-console.log(age1 > age2);  // false
-console.log(age1 < age2);  // false
+console.log(age1 > age2); // false
+console.log(age1 < age2); // false
 console.log(age1 >= age2); // true
 console.log(age1 <= age2); // true
 console.log(age1 === age2); // true
+
+if (age1 >= 18 || age2 >= 18) {
+    console.log("You are eligible to vote..");
+}
+else {
+    console.log("You are not eligible for vote..");
+    
+}
+console.log("End of Program");
