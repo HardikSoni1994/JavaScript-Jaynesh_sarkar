c50793a6a4c70f286226853aed3e9b3d57d6fbb4
HardikSoni1994 <soni.hardik994@gmail.com>
2026-01-18 21:08:30 +0530

    for Loop

---
+++ CH-6 Looping/6.2/forLoop.js
@@ -0,0 +1,18 @@
+console.log("Program Started..");
+
+let array = ["Audi", "BMW", "Mercedes", "Kia", "Porsche", "Bently", "Jaguar"];
+for (let i = 0; i < array.length; i++) {
+    console.log(`${i + 1}. ${array[i]}`);
+}
+console.log("----------------------------------");
+
+console.log("Even number find");
+
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log( i, "is even..");
+    }
+}
+console.log("Programm ended");
+
+
