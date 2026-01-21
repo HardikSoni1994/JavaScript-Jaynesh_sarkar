3d42571a0ae504a05adb11a188f26274f1b22505
HardikSoni1994 <soni.hardik994@gmail.com>
2026-01-21 23:18:06 +0530

    Function

--- CH-7 Function/function.js
+++ CH-7 Function/function.js
@@ -2,3 +2,12 @@ function myName() {
     console.log("Dev. Hardik Soni");
 }
 myName();
+
+function user(username = 'NULL') {  // default parameter
+    console.log(`My Name is ${username}`);
+    console.log("I'm a Full Stack Developer.");    
+}
+console.log("---------------------------");
+user("Hardik");
+console.log("---------------------------");
+user();
