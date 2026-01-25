038d407a95a1ad908e16748af5fc196f0b6fad55
HardikSoni1994 <soni.hardik994@gmail.com>
2026-01-25 12:04:59 +0530

    return value in function

--- CH-7 Function/function.js
+++ CH-7 Function/function.js
@@ -13,3 +13,21 @@ console.log("---------------------------");
 userDetails("Hardik", "Full Stack Developer", 27);
 console.log("---------------------------");
 userDetails(); // DEFAULT value passed in argument.(default parameter undefined value passed)
+
+// Addition
+function sumOfTwoNUmber(a, b) {
+    return a +b; // return value
+}
+let addition = sumOfTwoNUmber(4, 8);
+console.log("sum of value of a + b is equal to", addition);
+
+// Substraction
+function substractionOfTwoNUmber(a, b) {
+    return a - b;
+}
+let substraction = substractionOfTwoNUmber(10, 8);
+console.log("sum of value of a - b is equal to", substraction);
+
+let ans = sumOfTwoNUmber(sumOfTwoNUmber(5, 10), sumOfTwoNUmber(12, 8));
+
+console.log(ans);
